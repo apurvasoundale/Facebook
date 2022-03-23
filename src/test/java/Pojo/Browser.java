ed37998721081ce3ecce7255c97ec7c8ace5eef4
DELL <DELL@DESKTOP-Q5P53P5>
2022-03-23 12:44:47 +0530

    Changes in Pojo

--- src/test/java/Pojo/Browser.java
+++ src/test/java/Pojo/Browser.java
@@ -44,14 +44,7 @@ public class Browser {
 		return driver;
 	}
 	
-	public static WebDriver launchIEBrowser()
-	{
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe");
-		
-		WebDriver driver = new FirefoxDriver();
-		
-		return driver;
-	}
+	
 	
 	
 
